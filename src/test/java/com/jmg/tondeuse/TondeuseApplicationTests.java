1734e4afa93858c2bead62ec33da13f4da9b821f
Jean-Michel <jeanmichelgadeaud@hotmail.fr>
2025-06-26 14:49:25 +0200

    init projet de spring.io

---
+++ src/test/java/com/jmg/tondeuse/TondeuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmg.tondeuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TondeuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
